9cc3dda1082ae2719166d805a33d2bea811cc087
Victor <vmartina@iesch.org>
2022-10-04 20:48:18 +0200

    investigar lo de los dialogos

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.mycalculator;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-
+import android.app.Dialog;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView etxNumber1, etxNumber2;
     private ActivityMainBinding binding;
 
+//    @Override
+//    public Dialog onCreateDialog(Bundle savedInstanceState){
+//        AlertDialog.Builder adBuilder = new AlertDialog.Builder();
+//        return null;
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         if (etxNumber1 != null) {
             binding.etxNumber1.getText();
         }else{
-            //MOSTRAR MENSAJE EMERGENTE "FALTA INTRODUCIR N1".
+            //MOSTRAR DIALOGO EMERGENTE "FALTA INTRODUCIR N1".
         }
     }
 }
