be84d6f186ca7e3a89edece701c98fd2fa759096
martoKZER <victormartinandres@gmail.com>
2022-10-14 14:36:48 +0200

    YA CHUTA. ARREGLAR DIVISION ENTRE 0 EN CLASE

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -4,28 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.example.mycalculator.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
-
     //En el metodo onCreate irá el código que se tiene que ejecutar al abrir la aplicación
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         EditText etxNumber1 = binding.etxNumber1;
         EditText etxNumber2 = binding.etxNumber2;
-
-        TextView tvResult = binding.tvResult;
-        binding.btnSumar.setOnClickListener(v -> {
+        EditText tvResult = binding.tvResult;
+        Button btnSumar = binding.btnSumar;
+        Button btnRestar = binding.btnRestar;
+        Button btnMult = binding.btnMult;
+        Button btnDiv = binding.btnDiv;
+        tvResult.setEnabled(false);
+        btnSumar.setOnClickListener(v -> {
             if (etxNumber1.getText() != null || etxNumber2.getText() != null) {
                 try {
-                    int n1 = Integer.parseInt(String.valueOf(etxNumber1));
-                    int n2 = Integer.parseInt(String.valueOf(etxNumber2));
+                    int n1 = Integer.parseInt(String.valueOf(etxNumber1.getText()));
+                    int n2 = Integer.parseInt(String.valueOf(etxNumber2.getText()));
                     int res = n1 + n2;
                     String resultado = Integer.toString(res);
                     tvResult.setText(resultado);
@@ -33,20 +35,24 @@ public class MainActivity extends AppCompatActivity {
                 } catch (NumberFormatException e) {
                     e.printStackTrace();
                 }
-            } else {
-                // Ir a otra actividad PASO 1.
-                Intent intent = new Intent(MainActivity.this, WhitespaceActivity.class);
-                intent.putExtra("@string/whitespace", 0);
-                MainActivity.this.startActivity(intent);
-
             }
         });
 
-        binding.btnRestar.setOnClickListener(v -> {
+        btnRestar.setOnClickListener(v -> {
             if (etxNumber1.getText() != null || etxNumber2.getText() != null) {
-                int n1 = Integer.parseInt(String.valueOf(etxNumber1));
-                int n2 = Integer.parseInt(String.valueOf(etxNumber2));
-                String resultado = Integer.toString(n1 - n2);
+                int n1 = Integer.parseInt(String.valueOf(etxNumber1.getText()));
+                int n2 = Integer.parseInt(String.valueOf(etxNumber2.getText()));
+                int res = n1 - n2;
+                String resultado = Integer.toString(res);
+                tvResult.setText(resultado);
+            }
+        });
+        btnMult.setOnClickListener(v -> {
+            if (etxNumber1.getText() != null || etxNumber2.getText() != null) {
+                int n1 = Integer.parseInt(String.valueOf(etxNumber1.getText()));
+                int n2 = Integer.parseInt(String.valueOf(etxNumber2.getText()));
+                int res = n1 * n2;
+                String resultado = Integer.toString(res);
                 tvResult.setText(resultado);
             } else {
                 // Ir a otra actividad PASO 1.
@@ -56,31 +62,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        binding.btnMult.setOnClickListener(v -> {
+        btnDiv.setOnClickListener(v -> {
             if (etxNumber1.getText() != null || etxNumber2.getText() != null) {
-                int n1 = Integer.parseInt(String.valueOf(etxNumber1));
-                int n2 = Integer.parseInt(String.valueOf(etxNumber2));
-                String resultado = Integer.toString(n1 * n2);
+                int n1 = Integer.parseInt(String.valueOf(etxNumber1.getText()));
+                int n2 = Integer.parseInt(String.valueOf(etxNumber2.getText()));
+                int res = n1/n2;
+                String resultado = Integer.toString(res);
                 tvResult.setText(resultado);
-            } else {
-                // Ir a otra actividad PASO 1.
-                Intent intent = new Intent(MainActivity.this, WhitespaceActivity.class);
-                intent.putExtra("@string/whitespace", 0);
-                MainActivity.this.startActivity(intent);
 
             }
-        });
-        binding.btnDiv.setOnClickListener(v -> {
-            if (etxNumber1.getText() != null || etxNumber2.getText() != null) {
-                int n1 = Integer.parseInt(String.valueOf(etxNumber1));
-                int n2 = Integer.parseInt(String.valueOf(etxNumber2));
-                String resultado = Integer.toString(n1 / n2);
-                tvResult.setText(resultado);
-            } else {
-                // Ir a otra actividad PASO 1.
-                Intent intent = new Intent(MainActivity.this, WhitespaceActivity.class);
-                intent.putExtra("@string/whitespace", 0);
-                MainActivity.this.startActivity(intent);
+            if (etxNumber2.getText().equals(0)){
+                tvResult.setText("@string/div0");
             }
         });
     }
